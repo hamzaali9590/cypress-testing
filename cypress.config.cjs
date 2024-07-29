239354d9528c09e916ce1d348001fcc2b3dc2257
hamzaali9590 <hamzaali9590@hotmail.com>
2024-07-29 11:17:41 +0500

    login spec checked in

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    frontendBaseUrl: 'http://localhost:3000',
+    backendBaseUrl : 'http://localhost:5001',
     setupNodeEvents(on, config) {      
     }
   },
--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -1,74 +1,93 @@
-// Authenticates user with correct credentails on backend
+// cypress/integration/login.spec.js
+/// <reference types="cypress" />
 
-describe('The Login Page', () => {
-  it('should authenticate user with valid credentials', () => {
-  cy.request({
-    method: 'POST',
-    url: 'http://localhost:3000/login',
-    body: {
-      userName: 'validUser',
-      password: 'validPassword123'
-    },
-    failOnStatusCode: false 
-  }).then((response) => {
-    expect(response.status, 'status').to.equal(201)
-    expect(response.body).to.deep.equal('Login successful')
-  })
-      cy.visit('http://localhost:3000')
-    })
+describe('Login Page', () => {
+  
+  beforeEach(() => {
+    cy.visit(Cypress.config().frontendBaseUrl + '/login');
+    
+    cy.window().then((win) => {
+       win.document.body.style.zoom = '100%';
+       expect(win.document.body.style.zoom).to.equal('100%');
+     });
+  });
+
+  it('should display the login form', () => {
+    cy.get('.login-container').should('be.visible');
+    cy.get('.login-form-container').should('be.visible');
+    cy.get('.login-password-field').should('be.visible');
+    cy.get('.login-email-field').should('be.visible');
+    cy.get('.btn--login').should('be.visible', );
+  });
+
+   it('should login successfully with valid credentials', () => {
+    cy.fixture('login').then((loginValues) => {
+      cy.get('.login-email-field').type(loginValues.validEmail);
+      cy.get('.login-password-field').type(loginValues.validPassword);
+      cy.get('.btn--login').click();
+  
+      cy.contains(loginValues.validLoginMessage).should('be.visible');
+
+      cy.url().should('eq', Cypress.config().frontendBaseUrl + '/');
+      cy.wait(5000);
+    });
+  });
+
+  it('should show an error message with invalid password', () => {
+    cy.fixture('login').then((loginValues) => {
+      cy.get('.login-email-field').type(loginValues.validEmail);
+      cy.get('.login-password-field').type(loginValues.invalidPassword);
+      cy.get('.btn--login').click();
+      
+      cy.contains(loginValues.invalidLoginMessage).should('be.visible');
+      cy.wait(3000);
+    });
+  });
+
+  it('should show an error message with invalid credentials', () => {
+    cy.fixture('login').then((loginValues) => {
+      cy.get('.login-email-field').type(loginValues.doesNotExistEmail);
+      cy.get('.login-password-field').type(loginValues.invalidPassword);
+      cy.get('.btn--login').click();
+      
+      cy.contains(loginValues.invalidLoginMessage).should('be.visible');
+      cy.wait(3000);
+    });
+  });
+
+  it('should show an error message with empty password', () => {
+    cy.fixture('login').then((loginValues) => {
+      cy.get('.login-email-field').type(loginValues.validEmail);
+
+      cy.get('.btn--login').click();
+      
+      cy.contains(loginValues.invalidLoginMessage).should('be.visible');
+      cy.wait(3000);
+    });
+  });
+
+  it('should show an error message with empty email', () => {
+    cy.fixture('login').then((loginValues) => {
+      cy.get('.login-password-field').type(loginValues.validPassword);
+      cy.get('.btn--login').click();
+      
+      cy.contains(loginValues.invalidLoginMessage).should('be.visible');
+      cy.wait(3000);
+    });
+  });
+
+  it('should show an error for invalid email format', () => {
+    cy.fixture('login').then((loginValues) => {
+      cy.get('.login-email-field').type(loginValues.invalidEmail);
+      cy.get('.login-password-field').type(loginValues.validPassword);
+      cy.get('.btn--login').click();
+
+      cy.contains(loginValues.invalidEmailMessage).should('be.visible');
+    });
+
+  });
+
 });
 
 
-// Authenticates user with wrong credentails on backend 
-
-// describe('The Login Page', () => {
-//   it('login attempt with wrong credentials', () => {
-//   cy.request({
-//     method: 'POST',
-//     url: 'http://localhost:3000/login',
-//     body: {
-//       userName: 'invalidUser',
-//       password: 'wrongPassword'
-//     },
-//     failOnStatusCode: false 
-//   }).then((response) => {
-//     expect(response.status, 'status').to.equal(401)
-//     expect(response.body).to.deep.equal('User name or password is not correct')
-//   })
-//     })
-// });
-
-
-
-// Authenticates user with correct credentails on frontend
-
-// describe('The Login Page', () => {
-//   it('Authentication successful', () => {
-//       let userName = 'validUser';
-//       let password = 'validPassword123';
-//       cy.visit('http://localhost:5173/', {failOnStatusCode: false});
-//       cy.get('form')
-//       cy.get('input[class=inputUserNameField]').type(userName)      
-//       cy.get('input[class=inputPasswordField]').type(password)
-//       cy.get('input[type=submit]').click();
-           
-//       cy.url().should('include', '/dashboard')
-//     })
-// })
-
-
-
-// Authenticates user with wrong credentails on frontend 
-// describe('The Login Page', () => {
-//   it('Authentication unsuccessful', () => {
-//       cy.visit('http://localhost:5173/', {failOnStatusCode: false});
-//       cy.get('form')
-//       cy.get('input[class=inputUserNameField]').type('invalidUser')      
-//       cy.get('input[class=inputPasswordField]').type('wrongPassword')
-//       cy.get('input[type=submit]').click();
-      
-      
-//       // cy.url().should('include', '/dashboard')
-//     })
-// })
 
