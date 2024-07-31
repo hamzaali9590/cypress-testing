2a2517b915948509cf97535f1d719492c319e11f
hamzaali9590 <hamzaali9590@hotmail.com>
2024-07-31 20:47:44 +0500

    Engagement changes

--- cypress/e2e/Engagements_second.cy.js
+++ cypress/e2e/Engagements_second.cy.js
@@ -1,44 +1,38 @@
 describe('The Engagement Page', () => {
-  const loginUrl = 'http://localhost:3000/login';
-  const baseUrl = 'http://localhost:3000/overview/my-engagements';
-  const userName = 'hamza.ali@srjca.com';
-  const password = 'password';
-  it('Logins user', () => {
+  beforeEach(function() {
+    cy.fixture('user').then((user) => {
+      this.user = user;
+    });
+  });
 
-    cy.visit(loginUrl, { failOnStatusCode: false });
-    cy.get('form')
-    cy.get('input[type=text]').type(userName, { force: true })
-    cy.get('input[type=password]').type(password, { force: true })
+  it('Logins user', function() {
+    const baseUrlFrontend = Cypress.config('frontendBaseUrl');
+    cy.visit(`${baseUrlFrontend}/login`, { failOnStatusCode: false });
+    cy.get('form');
+    cy.get('input[type=text]').type(this.user.userName, { force: true });
+    cy.get('input[type=password]').type(this.user.password, { force: true });
     cy.get('.btn--login').click({ force: true });
-    cy.url().should('include', '/')
+    cy.url().should('include', '/');
 
-    cy.get('.navbar-link').contains('My Engagements').click()
-    cy.url().should('include', baseUrl)
-    cy.get('.views-container').should('have.css', 'padding', '15px')
-    cy.get('.views-container__arrow').click({ force: true })
-    cy.get('.views-container').should('have.css', 'padding', '0px')
-    cy.get('.views-container__arrow').click({ force: true })
-    cy.wait(2000)
-    cy.get(".views-container__list__item").eq(1).click({ force: true })
-    cy.wait(2000)
-    cy.url().should('include', 'http://localhost:3000/workflow/t2')
-    cy.wait(2000)
-    cy.get(".views-container__list__item").eq(2).click({ force: true })
-    cy.wait(4000)
-
+    cy.get('.navbar-link').contains('My Engagements').click();
+    cy.url().should('include', `${baseUrlFrontend}/overview/my-engagements`);
+    cy.get('.views-container').should('have.css', 'padding', '15px');
+    cy.get('.views-container__arrow').click({ force: true });
+    cy.get('.views-container').should('have.css', 'padding', '0px');
+    cy.get('.views-container__arrow').click({ force: true });
+    cy.wait(2000);
+    cy.get(".views-container__list__item").eq(1).click({ force: true });
+    cy.wait(2000);
+    cy.url().should('include', `${baseUrlFrontend}/workflow/t2`);
+    cy.wait(2000);
+    cy.get(".views-container__list__item").eq(2).click({ force: true });
+    cy.wait(4000);
 
     cy.get('div').contains('Total Engagements: ').invoke('text').then((text) => {
       const totalEngagements = parseInt(text.slice(18).trim(), 10);
       cy.get('.list-column--xs').filter(':contains("Team A")').should('have.length', totalEngagements);
     });
 
-
-    cy.get('.container-add').eq(1).click({ force: true })
-
-
-  })
-
-
-
-
-})
+    cy.get('.container-add').eq(1).click({ force: true });
+  });
+});
